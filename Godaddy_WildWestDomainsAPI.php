188400e5461e6978ea3c855292a36ac03638bb27
Jeremi Bergman <jeremib@gmail.com>
2014-12-09 18:07:43 -0500

    New Modify nameserver

--- Godaddy_WildWestDomainsAPI.php
+++ Godaddy_WildWestDomainsAPI.php
@@ -9,5 +9,28 @@ function godaddy_wwd_autoload($class) {
 spl_autoload_register('godaddy_wwd_autoload');
 
 class Godaddy_WildWestDomainsAPI extends \WildWest_Reseller_Client{
+    /**
+     * Gets information about items that have been previously ordered.
+     *
+     * @return
+     */
+    public function ModifyNameServer(array $domains = array(), array $nameservers = array())
+    {
+        $data = array(
+            'credential' => $this->_credential,
+            'sCLTRID' => $this->getClientTransactionId(),
+            'domainArray' => $domains,
+            'nsArray' => $nameservers
+        );
 
+        $response = $this->__call('UpdateNameServer', array($data));
+
+        $xml  = new SimpleXMLElement($response->UpdateNameServerResult);
+
+        if (empty($xml->resdata)) {
+            throw new WildWest_Reseller_Exception((string)$xml->result->msg->error, (string)$xml->result['code']);
+        } else {
+            return (string)$xml->code == 1000;
+        }
+    }
 }
